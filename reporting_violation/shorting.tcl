ffe2f4ec6e0940d4a2c5132710631fb07d2048a9
vinaykumar3096 <vinaykumar3096@gmail.com>
2021-06-22 11:59:08 +0530

    bug updated bu vinay kumawat

--- reporting_violation/shorting.tcl
+++ reporting_violation/shorting.tcl
@@ -7,20 +7,18 @@ set flag 0
 while { [ gets $FR line ] >= 0 } {
         if { [ regexp "Startpoint" $line ] } {
                 set flag 1
-        } elseif { [ regexp "Slack" $line ] } {
+        } elseif { [ regexp "slack" $line ] } {
                 lappend data "$line \n"
                 set flag 0
+		if { [ regexp "VIOLATED" $line ] } {
+                     puts $data
+                     set data ""
+                } elseif { [ regexp "MET" $line ] } {
+                     set data ""
+                }
         }
         if { $flag == 1 } {
-                lappend data "$line \n"
-        }
-        if { [ regexp "VIOLATED" $line ] } {
-                puts $data
-                set data ""
-		set flag 0
-        } elseif { [ regexp "MET" $line ] } {
-                set data ""
-		set flag 0
+               lappend data "$line \n"
         }
 }
 
